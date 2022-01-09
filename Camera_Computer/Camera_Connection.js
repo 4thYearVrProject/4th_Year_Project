e8e243e61b6c89e1b03b9648f92ee35785cffd8b
Jayton McCracken <jmccrac@bell.net>
2022-01-09 12:39:05 -0500

    Streaming through Localhost
    Creation of a server to run a localhost on port 4040, able to broadcast and watchers can join on to view

---
+++ Camera_Computer/Camera_Connection.js
@@ -0,0 +1,90 @@
+const peerConnections = {};
+const config = {
+  iceServers: [
+    { 
+      "urls": "stun:stun.l.google.com:19302",
+    },
+    // { 
+    //   "urls": "turn:TURN_IP?transport=tcp",
+    //   "username": "TURN_USERNAME",
+    //   "credential": "TURN_CREDENTIALS"
+    // }
+  ]
+};
+
+const socket = io.connect(window.location.origin);
+
+socket.on("answer", (id, description) => {
+  peerConnections[id].setRemoteDescription(description);
+});
+
+socket.on("watcher", id => {
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
+
+  let stream = videoElement.srcObject;
+  stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
+
+  peerConnection
+    .createOffer()
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      socket.emit("offer", id, peerConnection.localDescription);
+    });
+});
+
+socket.on("candidate", (id, candidate) => {
+  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+});
+
+socket.on("disconnectPeer", id => {
+  peerConnections[id].close();
+  delete peerConnections[id];
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+};
+
+getStream()
+  .then(getDevices)
+  .then(gotDevices);
+
+function getStream() {
+    if (window.stream) {
+      window.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+    const audioSource = audioSelect.value;
+    const videoSource = videoSelect.value;
+    const constraints = {
+      audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+      video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    };
+    return navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(gotStream)
+      .catch(handleError);
+  }
+function gotStream(stream) {
+    window.stream = stream;
+    audioSelect.selectedIndex = [...audioSelect.options].findIndex(
+      option => option.text === stream.getAudioTracks()[0].label
+    );
+    videoSelect.selectedIndex = [...videoSelect.options].findIndex(
+      option => option.text === stream.getVideoTracks()[0].label
+    );
+    videoElement.srcObject = stream;
+    socket.emit("broadcaster");
+  }
+
+function handleError(error) {
+  console.error("Error: ", error);
+}
---
+++ Camera_Computer/Camera_Start.js
@@ -0,0 +1,27 @@
+// Get camera and microphone
+const videoElement = document.querySelector("video");
+const audioSelect = document.querySelector("select#audioSource");
+const videoSelect = document.querySelector("select#videoSource");
+
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+function getDevices() {
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos;
+  for (const deviceInfo of deviceInfos) {
+    const option = document.createElement("option");
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === "audioinput") {
+      option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
+      audioSelect.appendChild(option);
+    } else if (deviceInfo.kind === "videoinput") {
+      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    }
+  }
+}
+
