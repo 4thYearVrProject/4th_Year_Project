e76c04093d39a391147abe79a242982eaa4e0bcb
Matthew Harris <1999mrh@gmail.com>
2022-02-11 16:46:40 -0500

    Revert "Merge pull request #5 from 4thYearVrProject/WebXR"
    This reverts commit b5f76a8a656c9353d33d0808bd0d97babf1f142a, reversing
    changes made to c7641cf5b2517a7da611702e8ab68b1a8768b04c.

--- Camera_Computer/Camera_Terminate_Connection.js
+++ Camera_Computer/Camera_Terminate_Connection.js
@@ -1,1 +1,9 @@
-
+/**
+ * Performs the termination after a disconnection
+ */
+socket.on("disconnectPeer", (id) => {
+  peerConnections[id].close();
+  numberOfWatchers -= 1;
+  updateConnection(peerConnections[id], numberOfWatchers);
+  delete peerConnections[id];
+});
--- Headset_Computer/HS_Connection.js
+++ Headset_Computer/HS_Connection.js
@@ -1,61 +1,70 @@
 let peerConnection;
+// The RTC configuration settings
 const config = {
   iceServers: [
-      { 
-        "urls": "stun:stun.l.google.com:19302",
-      },
-      // { 
-      //   "urls": "turn:TURN_IP?transport=tcp",
-      //   "username": "TURN_USERNAME",
-      //   "credential": "TURN_CREDENTIALS"
-      // }
-  ]
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    // {
+    //   "urls": "turn:TURN_IP?transport=tcp",
+    //   "username": "TURN_USERNAME",
+    //   "credential": "TURN_CREDENTIALS"
+    // }
+  ],
 };
 
 const socket = io.connect(window.location.origin);
 const video = document.querySelector("video");
 
-
+// Creates the peer side connection after receiving "offer" signal
 socket.on("offer", (id, description) => {
   peerConnection = new RTCPeerConnection(config);
   peerConnection
     .setRemoteDescription(description)
     .then(() => peerConnection.createAnswer())
-    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then((sdp) => peerConnection.setLocalDescription(sdp))
     .then(() => {
       socket.emit("answer", id, peerConnection.localDescription);
     });
-  peerConnection.ontrack = event => {
+  peerConnection.ontrack = (event) => {
     video.srcObject = event.streams[0];
   };
-  peerConnection.onicecandidate = event => {
+  peerConnection.onicecandidate = (event) => {
     if (event.candidate) {
       socket.emit("candidate", id, event.candidate);
     }
   };
 });
 
-
+// On selection of canidate by host, sets the RTC canidate
 socket.on("candidate", (id, candidate) => {
   peerConnection
     .addIceCandidate(new RTCIceCandidate(candidate))
-    .catch(e => console.error(e));
+    .catch((e) => console.error(e));
 });
 
+// On connection, emits a "watcher" signal to the host
 socket.on("connect", () => {
   socket.emit("watcher");
 });
 
+// On connection, emits a "watcher" signal to the host
 socket.on("broadcaster", () => {
   socket.emit("watcher");
 });
 
+/**
+ * On closing the page, closes the socket and connection
+ */
 window.onunload = window.onbeforeunload = () => {
   socket.close();
   peerConnection.close();
 };
 
+/**
+ * Enables the audio to play
+ */
 function enableAudio() {
-  console.log("Enabling audio")
+  console.log("Enabling audio");
   video.muted = false;
 }
