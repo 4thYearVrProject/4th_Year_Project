604d7cb5da0fe2e34f01bccbdd44e62506bafe27
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-02-25 17:47:36 -0500

    add jsDoc comments
    refactor setupController

--- Headset_Computer/HS_VREnviroment.js
+++ Headset_Computer/HS_VREnviroment.js
@@ -2,10 +2,21 @@ import * as THREE from '../Open_Source_Code/three.js/build/three.module.js';
 import { VRButton } from '../Open_Source_Code/three.js/VRButton.js';
 import { XRControllerModelFactory } from '../Open_Source_Code/three.js/XRControllerModelFactory.js';
 import '../Headset_Computer/HS_Connection.js';
-//import WebXRPolyfill from './js/third-party/webxr-polyfill/build/webxr-polyfill.module.js';
+
 let renderer, camera, scene;
 
+/**
+ * The VR Environment is used to render the video from the html streams
+ * onto the VR headset. Also renders controllers, maps their inputs, and
+ * renders guide lights
+ */
 class VREnviroment {
+    /**
+     * Creates a VR Environment that is used to render video from
+     * html stream onto the VR headset.
+     *
+     * @return {VREnviroment}  Returns a VREnvironment
+     */
     constructor() {
         const container = document.getElementById('container');
         container.addEventListener('click', function () {
@@ -13,7 +24,7 @@ class VREnviroment {
         });
 
         camera = new THREE.PerspectiveCamera(
-            70,
+            70, // FOV
             window.innerWidth / window.innerHeight,
             0.1,
             2000
@@ -26,7 +37,6 @@ class VREnviroment {
         const video = document.getElementById('Video');
         video.play();
         const texture = new THREE.VideoTexture(video);
-        
 
         // Creates a mesh of the video, and adds them to the scene
         this.createMesh('left', texture);
@@ -45,19 +55,21 @@ class VREnviroment {
         const vrButton = document.getElementById('vrButton');
         vrButton.appendChild(VRButton.createButton(renderer));
 
-        this.animate();
+        renderer.setAnimationLoop(this.update);
 
         window.addEventListener('resize', this.onWindowResize);
     }
 
-    animate() {
-        renderer.setAnimationLoop(this.update);
-    }
-
+    /**
+     * Renders the scene, is done every frame
+     */
     update() {
         renderer.render(scene, camera);
     }
 
+    /**
+     * changes the aspect ratio on window resizing
+     */
     onWindowResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
@@ -65,51 +77,95 @@ class VREnviroment {
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
+    /**
+     * Sets up controllers so that they are tracked, inputs are mapped
+     * and guidelines are attached
+     */
     setUpController() {
-
-        // Get the left controller
         const controllerModelFactory = new XRControllerModelFactory();
-        const leftController = renderer.xr.getController(0);
-        const leftModel = controllerModelFactory.createControllerModel(
-            leftController
-        );
-        leftController.add(leftModel);
-        
-        
-        leftController.addEventListener('select', leftTriggerButtonResponse);
-        leftController.addEventListener('squeeze', leftSqueezeButtonResponse);
+        let leftController = getController('left', controllerModelFactory);
+        let rightController = getController('right', controllerModelFactory);
 
-        // Get the right controller
-        const rightController = renderer.xr.getController(1);
-        const rightModel = controllerModelFactory.createControllerModel(
-            rightController
-        );
-        rightController.add(rightModel);
-       
-        
-        rightController.addEventListener('select', rightTriggerButtonResponse);
-        rightController.addEventListener('squeeze', rightSqueezeButtonResponse);
-
-        //Setting up the guide lines
-        const lineSegments=10;
-        const lineGeometryVertices = new Float32Array((lineSegments +1) * 3);
-        const lineGeometry = new THREE.BufferGeometry();
-        lineGeometryVertices.fill(0);
-        const lineGeometryColors = new Float32Array((lineSegments +1) * 3);
-        lineGeometryColors.fill(0.5);
-        lineGeometry.setAttribute('position', new THREE.BufferAttribute(lineGeometryVertices, 3));
-        lineGeometry.setAttribute('color', new THREE.BufferAttribute(lineGeometryColors, 3));
-        const lineMaterial = new THREE.LineBasicMaterial({ color: 0x888888, blending: THREE.AdditiveBlending });
-        const guideline = new THREE.Line( lineGeometry, lineMaterial );
-        const guideLight = new THREE.PointLight(0xffffff, 0, 2);
-        guideLight.intensity = 1;
-        rightController.add(guideline);
+        let guideline = getGuideLine();
         leftController.add(guideline);
-        scene.add(rightController);
-        scene.add(leftController);
+        rightController.add(guideline);
     }
 
+    /**
+     * gets the controller and maps its buttons to corresponding functions
+     *
+     * @param  {string} side  The side the controller is on, left or right
+     * @param  {ControllerModelFactory} controllerModelFactory
+     * @return {Group}   A representation of the controller
+     */
+    getController(side, controllerModelFactory) {
+        const controllerIndex = side == 'left' ? 0 : 1;
+        const controller = renderer.xr.getController(controllerIndex);
+        const model = controllerModelFactory.createControllerModel(controller);
+        controller.add(model);
+
+        controller.addEventListener(
+            'select',
+            window[side + 'TriggerButtonResponse']
+        );
+        controller.addEventListener(
+            'squeeze',
+            window[side + 'SqueezeButtonResponse']
+        );
+        scene.add(controller);
+    }
+
+    /**
+     * Gets a guideline for displaying on the end of the controller
+     * Used for pointing at specific locations with a controller
+     *
+     * @return {Line}  a THREE.Line that can be attached to a controller
+     */
+    getGuideLine() {
+        const lineSegments = 10;
+        const lineGeometryVertices = new Float32Array((lineSegments + 1) * 3);
+        const lineGeometry = new THREE.BufferGeometry();
+        lineGeometryVertices.fill(0);
+        const lineGeometryColors = new Float32Array((lineSegments + 1) * 3);
+        lineGeometryColors.fill(0.5);
+        lineGeometry.setAttribute(
+            'position',
+            new THREE.BufferAttribute(lineGeometryVertices, 3)
+        );
+        lineGeometry.setAttribute(
+            'color',
+            new THREE.BufferAttribute(lineGeometryColors, 3)
+        );
+        const lineMaterial = new THREE.LineBasicMaterial({
+            color: 0x888888,
+            blending: THREE.AdditiveBlending,
+        });
+        const guideline = new THREE.Line(lineGeometry, lineMaterial);
+
+        return guideline;
+    }
+
+    /**
+     * Gets a point light that is used to display the
+     * location the guidlines meet terrain
+     *
+     * @return {PointLight}  a THREE.PointLight that highlights a point
+     */
+    getGuideLight() {
+        const guideLight = new THREE.PointLight(0xffffff, 0, 2);
+        guideLight.intensity = 1;
+
+        return guideLight;
+    }
+
+    /**
+     * Creates the geometry mesh that will render the video texure around each eye
+     *
+     * @param  {string} side    the eye the mesh is displayed for, left or right
+     * @param  {type} texture   the texture that will be mapped onto the mesh
+     */
     createMesh(side, texture) {
+        // radius, widthSegments, heightSegments
         const geometry = new THREE.SphereGeometry(200, 60, 40);
         const material = new THREE.MeshBasicMaterial({ map: texture });
 
@@ -117,18 +173,24 @@ class VREnviroment {
         geometry.scale(-1, 1, 1);
 
         const uvs = geometry.getAttribute('uv').array;
-
         for (let i = 0; i < uvs.length; i += 2) {
-            uvs[i] = side == 'left' ? uvs[i] * 0.5 : uvs[i] * 0.5;// + 0.5;
+            uvs[i] = side == 'left' ? uvs[i] * 0.5 : uvs[i] * 0.5; // + 0.5;
         }
 
         const mesh = new THREE.Mesh(geometry, material);
         mesh.rotation.y = -Math.PI / 2;
+
+        // Layers 0 & 1 are rendered for left eye
+        // Layers 0 & 2 are rendered for right eye
         let layer = side == 'left' ? 1 : 2;
         mesh.layers.set(layer);
         scene.add(mesh);
     }
 }
+
+/**
+ *  Functions send messages over socket to camera computer
+ */
 function leftTriggerButtonResponse() {
     sendMessage('trigger Button Pressed on left controller');
 }
@@ -141,4 +203,6 @@ function rightTriggerButtonResponse() {
 function rightSqueezeButtonResponse() {
     sendMessage('squeeze Button Pressed on right controller');
 }
+
+// Runs the VREnvirnment
 var vr = new VREnviroment();
